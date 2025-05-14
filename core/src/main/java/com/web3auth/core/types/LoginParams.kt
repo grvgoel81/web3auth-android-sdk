a0d5a2dc4265c7ce9fa505ed1fffea1c35629e9b
Gaurav Goel <gaurav@tor.us>
2025-05-14 11:42:05 +0530

    feat: Add new parameters in LoginParams.kt
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- core/src/main/java/com/web3auth/core/types/LoginParams.kt
+++ core/src/main/java/com/web3auth/core/types/LoginParams.kt
@@ -6,13 +6,13 @@ import androidx.annotation.Keep
 @Keep
 data class LoginParams(
     @Keep val authConnection: AuthConnection,
+    @Keep val authConnectionId: String? = null,
+    @Keep val groupedAuthConnectionId: String? = null,
     @Keep var dappShare: String? = null,
     @Keep val extraLoginOptions: ExtraLoginOptions? = null,
     @Keep @Transient var redirectUrl: Uri? = null,
     @Keep val appState: String? = null,
     @Keep val mfaLevel: MFALevel? = null,
     @Keep val curve: Curve? = Curve.SECP256K1,
-    @Keep val dappUrl: String? = null,
-    @Keep val authConnectionId: String? = null,
-    @Keep val groupedAuthConnectionId: String? = null
+    @Keep val dappUrl: String? = null
 )
